9a74c83b438ee98727447fbc09f96a87e25d02c9
difrodar <difrodar@students.zhaw.ch>
2026-04-01 14:38:21 +0200

    add component

---
+++ src/lib/components/test.svelte
@@ -0,0 +1,7 @@
+<script>
+    let {message, bla, name }= $props();
+</script>
+
+<p>Message: {message}</p>
+<p>bla: {bla}</p>
+<p>name: {name}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
+<Script>
+    import test from "$lib/components/test.svelte";
+</Script>
+
 <h1>Übung SW 6</h1>
+
+<test message="msg" ></test>
