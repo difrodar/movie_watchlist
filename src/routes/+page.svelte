a677617ec255aac11ec66eb258d0cd72b49c291d
difrodar <difrodar@students.zhaw.ch>
2026-04-01 14:41:29 +0200

    correction of component integration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
-<Script>
-    import test from "$lib/components/test.svelte";
-</Script>
+<script>
+    import TestBox from "$lib/components/test.svelte";
+</script>
 
 <h1>Übung SW 6</h1>
 
-<test message="msg" ></test>
+<div style="color:white">
+<TestBox message="msg" bla="hund" name="dario" ></TestBox>
+</div>
